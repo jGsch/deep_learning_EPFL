121663b0c3e05c48b8fc345cd26e08def3e5bccd
jGsch <jean.gschwindu@protonmaile.com>
2018-02-28 15:06:46 +0100

    pratical 2 continues...

--- PRATICALS/dlc-practical-2.py
+++ PRATICALS/dlc-practical-2.py
@@ -11,24 +11,23 @@ import dlc_practical_prologue as prologue
 def nearest_classification(train_input, train_target, x):
     distances = torch.norm(train_input - x, 2, 1)
     _ , indices = distances.sort()
-    return train_target[indices]
+    return train_target[indices[0]]
 
 
 # 2 Error estimation
 
 def compute_nb_errors(train_input, train_target, test_input, test_target, mean = None, proj = None):
     if mean is not None:
-        train_input = train_input - mean.view(mean.size()[0], 1).expand_as(train_input)
-        test_input  = test_input  - mean.view(mean.size()[0], 1).expand_as(test_input)
+        train_input = train_input - mean
+        test_input  = test_input - mean
 
     if proj is not None:
-        train_input = train_input * proj.transpose(1, 0)
-        test_input = test_input * proj.transpose(1, 0)
+        train_input = torch.mm(train_input, proj.transpose(1, 0))
+        test_input  = torch.mm(test_input, proj.transpose(1, 0))
 
     error = 0
     for index, test in enumerate(test_input):
-        label = nearest_classification(train_input, train_target, test)
-        if not label[0] == test_target[index]:
+        if not nearest_classification(train_input, train_target, test) == test_target[index]:
             error += 1
 
     return error
@@ -37,12 +36,31 @@ def compute_nb_errors(train_input, train_target, test_input, test_target, mean =
 # 3 PCA
 
 def PCA(x):
-    mean_x = 1/x.size()[0] * torch.sum(x, 1)
-    eigen_values = torch.mm(x.transpose(1, 0), x).eig()
+    mean = x.mean(0)
+    b = x - mean
 
+    eig_values, eig_vectors = torch.mm(x.transpose(1, 0), x).eig(True)
+    _ , order = eig_values[:, 0].abs().sort(0, True)
+    eig_vectors = eig_vectors.t()[order]
 
-N, D, C = 10, 4, 2
-train_input = torch.Tensor(N, D).normal_()
+    return mean, eig_vectors
 
 
 # 4 Check that all this makes sense
+
+for cifar in [False, True]:
+    if cifar: print("Cifar dataset...")
+    else: print("MNIST dataset...")
+
+    train_input, train_target, test_input, test_target = prologue.load_data(cifar = None)
+
+    basis = train_input.new(100, train_input.size(1)).normal_()
+    nb_error = compute_nb_errors(train_input, train_target, test_input, test_target, None, basis)
+    print("Number of error: " + str(nb_error/test_input.size(0)))
+
+    mean, basis = PCA(train_input)
+    train_input
+    for dimension in [3, 10, 50, 100]:
+        basis_pca = basis[0:dimension,:]
+        nb_error = compute_nb_errors(train_input, train_target, test_input, test_target, mean, basis_pca)
+        print("%_error: " + str(nb_error/test_input.size(0)) + " (Dimension: " + str(dimension) + ")")
